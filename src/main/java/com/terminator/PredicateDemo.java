5587e1ee92d236be5847dde1e0a316253f56d396
berna39 <kalemajoseph39@gmail.com>
2023-05-28 14:14:58 +0200

    feat :spakles: predicates chaining with AND

--- src/main/java/com/terminator/PredicateDemo.java
+++ src/main/java/com/terminator/PredicateDemo.java
@@ -13,5 +13,25 @@ public class PredicateDemo {
         people.stream().filter(predicate).forEach(p -> {
             System.out.println(p);
         });
+
+        // ------------- Predicate methods ------------- //
+
+        // test(T t) : Evaluates this predicate on the given argument.boolean test(T t)
+        Predicate<Integer> lesserthan = i -> (i < 18); 
+        System.out.println(lesserthan.test(13));
+        System.out.println(lesserthan.test(32));
+        
+        // ----- Predicate negate ------ //
+
+        // ----- and ------ //
+        // and(Predicate other) : Returns a composed predicate that represents a short-circuiting 
+        // logical AND of this predicate and another.
+        Predicate<Integer> greaterThanTen = (number) -> number > 10;
+        Predicate<Integer> lessThanTwenty = (number) -> number < 20;
+        System.out.println(greaterThanTen.and(lessThanTwenty).test(13)); // chaining predicates
+
+        // ----- Predicate or ------ //
+
+        // ----- Predicate equals ------ //
     }
 }
