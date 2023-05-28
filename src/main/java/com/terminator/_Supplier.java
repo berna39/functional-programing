f1d812a1037f1e52ec5e472732f2e0ca82eb9be6
berna39 <kalemajoseph39@gmail.com>
2023-05-28 17:06:46 +0200

    feat :sparkles: Supplier basics

---
+++ src/main/java/com/terminator/_Supplier.java
@@ -0,0 +1,18 @@
+package com.terminator;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class _Supplier {
+    
+    public static void main(String[] args) {
+        // A Supplier represents a function which does not take in any argument but produces a value of type T.
+        Supplier<Double> giveMeARandomNumber = () -> Math.random();
+        Supplier<List<String>> getDatabaseUrls = () -> List.of(
+            "jdbc://localhost:5432", "jdbc://localhost:3306");
+
+        System.out.println(giveMeARandomNumber.get());
+        System.out.println(getDatabaseUrls.get().get(0));
+        System.out.println(getDatabaseUrls.get().get(1));
+    }
+}
