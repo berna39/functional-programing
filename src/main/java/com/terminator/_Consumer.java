5dbac89004666cb8e14425c37498ad87c9161a38
berna39 <kalemajoseph39@gmail.com>
2023-05-28 16:50:05 +0200

    feat :sparkles: BiConsumer concept

--- src/main/java/com/terminator/_Consumer.java
+++ src/main/java/com/terminator/_Consumer.java
@@ -1,15 +1,21 @@
 package com.terminator;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
-    // a consumer accepts a single input and retuens not result
+    // A Consumer accepts a single input and returns not result
     public static void main(String[] args) {
         display.accept("AKATSUKI");
+        displayNames.accept("Obito", "Uchiwa");
     }
 
     static Consumer<String> display = (value) -> 
                     System.out.println(String.format("The value is : %s", value));
-                    
     // Signature Consumer<T>
+
+
+    // A BiConsumer accepts two input values and returns not result
+    static BiConsumer<String, String> displayNames = (name,  lastName) ->
+                    System.out.println(String.format("The name is : %s - %s", name, lastName));
 }
