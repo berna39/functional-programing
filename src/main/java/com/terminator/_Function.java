5b65271e309efc3bc7206d1a57470c71b7a862f1
berna39 <kalemajoseph39@gmail.com>
2023-05-28 14:45:00 +0200

    feat refacto :sparkes: initial demo of Fuction<T,R>

---
+++ src/main/java/com/terminator/_Function.java
@@ -0,0 +1,25 @@
+package com.terminator;
+
+import java.util.function.Function;
+
+public class _Function{
+    public static void main(String[] args) {
+        // Imperatif approach
+        int inc = increment(12);
+        System.out.println(inc);
+        
+        // Functional approach
+        System.out.println(advancedIncrement.apply(9));
+    }
+
+    // Represents a function that accepts one argument and produces a result.
+    static Function<Integer, Integer> advancedIncrement = number -> number + 1;
+
+    static Integer increment(int i){
+        // return i++; //never do this koz 
+        // return always evaluates the whole expression being returned (unless the return statement contains 
+        // some short circuited operator such as && and ||, but that's not the case here), but the evaluation of 
+        // the post increment operator a++ returns the old value of a.
+        return i + 1;
+    }
+}
