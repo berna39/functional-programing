b47267f4dea9d7e777e87d041595345925c71bf5
berna39 <kalemajoseph39@gmail.com>
2024-07-14 23:58:07 +0200

    :sparkles: Functions composition technique

---
+++ src/main/java/com/terminator/scenarios/_Composition.java
@@ -0,0 +1,15 @@
+package com.terminator.scenarios;
+
+import java.util.function.Function;
+
+public class _Composition {
+    
+    public static void main(String[] args) {
+        Function<String, String> trim = s -> s.trim();
+        Function<String, String> toUpperCase = s -> s.toUpperCase();
+        // the function passed in the argument executes first and then the function on which it’s invoked
+        Function<String, String> trimThenUpperCase = toUpperCase.compose(trim);
+        System.out.println(trimThenUpperCase.apply("      jude bellingham"));
+    }
+
+}
