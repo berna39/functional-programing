4413524a785d718fd8e76f194bc6d43b72daa634
berna39 <kalemajoseph39@gmail.com>
2024-08-17 22:23:46 +0200

    :lipstick: recursion refactoring

--- src/main/java/com/terminator/scenarios/_Recusion.java
+++ src/main/java/com/terminator/scenarios/_Recusion.java
@@ -1,24 +1,46 @@
 package com.terminator.scenarios;
 
 public class _Recusion {
-    
-    public static void main(String[] args) {
-        System.out.println(factorial(5));
-        System.out.println(factorial(5, 1));
-    }
 
     /*
-     * we’re making the recursive call before calculating the result at each step or in words
-     * at the head of the calculation. This style of recursion is also known as head recursion.
+     *  Best use cases: when I have a tree data structure and I need BACKTRACKING capabilities
+     *  Note: anything I can do with a recursion, I can do it with a loop and a Stack
      */
-    static Integer factorial(Integer number) {
+
+
+    /*
+     * we’re making the recursive call before calculating the result at each step. 
+     * This style of recursion is also known as head recursion.
+     */
+    public int factorial(int number) {
+        //      base case               recusive case
         return (number == 1) ? 1 : number * factorial(number - 1);
     }
 
     /*
-     * tail recursion technique -> more efficient
+     * tail recursion technique -> more efficient because we hold the n-1 execution result
+     *  so that we may not hold all the previous execution results
      */
-    static Integer factorial(Integer number, Integer result) {
+    public int factorial(int number, int result) {
+        //      base case               recusive case
         return (number == 1) ? result : factorial(number - 1, result * number);
+    }
+
+    public int sum(int number) {
+        // base case
+        if(number >= 1) {
+            //              recurvice case
+            return number + sum(number - 1);
+        }
+
+        return number;
+    }
+
+    public int sum(int param, int accumulator) {
+        if(param >= 1) {
+            return accumulator + param;
+        }
+
+        return sum(param - 1, accumulator + param);
     }
 }
---
+++ src/test/java/com/terminator/scenarios/_RecusionTest.java
@@ -0,0 +1,31 @@
+package com.terminator.scenarios;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class _RecusionTest {
+
+    private _Recusion recusionInstance;
+
+    @Before
+    public void setUp() {
+        recusionInstance = new _Recusion();
+    }
+
+    @Test
+    public void testFactorial_head_recursion_should_succeed() {
+        assertEquals(recusionInstance.factorial(5), 120);
+    }
+
+    @Test
+    public void testFactorial_tail_recursion_should_succeed() {
+         assertEquals(recusionInstance.factorial(5), 120);
+    }
+
+    @Test
+    public void testSum_should_succeed() {
+        assertEquals(recusionInstance.sum(4), 10);
+    }
+}
